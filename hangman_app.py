fcf78fbcd5e28c825beee4b3a8fe4a0f5c823877
jkgithub02 <jkwork02@gmail.com>
2024-08-15 15:43:48 +0800

    update

--- hangman_app.py
+++ hangman_app.py
@@ -19,7 +19,8 @@ def reset_game():
     st.session_state.user_letter = ""
 
 # Display the current game status
-st.title("Hangman Game")
+st.title("Kristang Hangman")
+st.write("1. 4 Kristang words are hidden, play a game of hangman to discover them and unlock the treasure!\n2. You have 6 lives, type a letter in the text box and double click the button to submit your guess!\n3. A wrong guess will result in one life gone, good luck!\n")
 
 if st.session_state.lives > 0 and any(word_letters for word_letters in st.session_state.word_list.values()):
     st.write(f'You have {st.session_state.lives} lives left.')
