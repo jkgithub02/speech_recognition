cce0c933f2c21d5948c18b7ed2dd7000a81e5c7c
jkgithub02 <jkwork02@gmail.com>
2024-08-21 20:18:05 +0800

    hangman update

--- hangman_app.py
+++ hangman_app.py
@@ -19,18 +19,14 @@ def reset_game():
     st.session_state.user_letter = ""
 
 # Display the current game status
-st.title("Kristang Hangman")
+st.title("Kristang Word Game- Hangman")
 st.write("1. 4 Kristang words are hidden, play a game of hangman to discover them and unlock the treasure!\n2. You have 6 lives, type a letter in the text box and double click the button to submit your guess!\n3. A wrong guess will result in one life gone, good luck!\n")
-
+st.write("HINT: The words in English are: sea, mud, tank and flag")
 if st.session_state.lives > 0 and any(word_letters for word_letters in st.session_state.word_list.values()):
     st.write(f'You have {st.session_state.lives} lives left.')
     st.write('Used letters:', ' '.join(st.session_state.used_letters))
     
-    for word in words:
-        word_display = [letter if letter in st.session_state.used_letters else '_' for letter in word]
-        st.write(' '.join(word_display))
-    
-    # Input for guessing a letter
+        # Input for guessing a letter
     user_letter = st.text_input("Guess a letter:", max_chars=1).lower()
     if st.button("Submit Guess"):
         if user_letter:
@@ -51,6 +47,12 @@ if st.session_state.lives > 0 and any(word_letters for word_letters in st.sessio
                 st.write('You have already used that letter.')
             else:
                 st.write('That is not a valid letter.')
+    
+    for word in words:
+        word_display = [letter if letter in st.session_state.used_letters else '_' for letter in word]
+        st.write(' '.join(word_display))
+    
+
 
 else:
     if st.session_state.lives == 0:
